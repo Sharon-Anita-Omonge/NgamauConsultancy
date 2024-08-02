e0941cac862455990f1ba2360f22cd598f2f4eb0
Sharon-Anita-Omonge <sharonanita2021@gmail.com>
2024-08-02 11:13:32 +0300

    hero

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import con1 from "../../assets/con1.jpg";
 import con2 from "../../assets/con2.jpg";
-import con3 from "../../assets/IT2.jpg";
 import con4 from "../../assets/con4.jpg";
 import con5 from "../../assets/con5.jpg";
 import "./Hero.css";
@@ -9,7 +8,6 @@ import "./Hero.css";
 const images = [
 	{ src: con1, caption: "Helping Companies Navigate Changes" },
 	{ src: con2, caption: "A Partner for Growth and Transformation" },
-	{ src: con3, caption: "Idea & Innovation in Every Step" },
 	{ src: con4, caption: "We Build Trust in Our Customer's Mind" },
 	{ src: con5, caption: "Ngamau Limited - Beyond Consulting" },
 ];
